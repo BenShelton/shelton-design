8f4ded8742b38008ecc96a0d35e00cfad198a8de
Ben Jones <bensheltonjones@gmail.com>
2018-02-23 16:35:33 +0000

    customise settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,3 @@
-// https://eslint.org/docs/user-guide/configuring
-
 module.exports = {
   root: true,
   parserOptions: {
@@ -9,17 +7,12 @@ module.exports = {
     browser: true,
   },
   extends: [
-    // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
-    // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    'plugin:vue/essential', 
-    // https://github.com/standard/standard/blob/master/docs/RULES-en.md
+    'plugin:vue/strongly-recommended', 
     'standard'
   ],
-  // required to lint *.vue files
   plugins: [
     'vue'
   ],
-  // add your custom rules here
   rules: {
     // allow async-await
     'generator-star-spacing': 'off',
